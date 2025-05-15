0690c75f0069c3213fd94f0f60e75ce640f5538c
shruthi-270706 <uligondashruthi@gmail.com>
2025-05-15 22:34:29 +0530

    my projects

---
+++ distance_ans.py
@@ -0,0 +1,4 @@
+from conversion import distance
+kilometers=int(input("Enter the Distance in Kilometers: "))
+meters_ans=distance.k_m(kilometers)
+print(f"{meters_ans} meters")
---
+++ vehicle/car_information.py
@@ -0,0 +1,4 @@
+def car(car_details):
+    return car_details
+def truck(truck_details):
+    return truck_details
