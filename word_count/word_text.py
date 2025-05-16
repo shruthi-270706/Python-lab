0d06eeb0a461bc21e9a08b2cee23735d01ffcaef
shruthi-270706 <uligondashruthi@gmail.com>
2025-05-16 21:48:54 +0530

    my projects

---
+++ word_count/word_text.py
@@ -0,0 +1,4 @@
+def word_count1(string):
+    words=string.split()
+    word_count=len(words)
+    return word_count
---
+++ word_countingans.py
@@ -0,0 +1,4 @@
+from word_count import word_text
+string=input("Enter the string: ")
+final_count=word_text.word_count1(string)
+print(final_count)
